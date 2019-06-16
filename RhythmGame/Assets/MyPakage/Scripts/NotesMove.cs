d487498e97f2d5c0f3708d18449cca3212a93552
1401341souama <uaebnncdeidie@gmail.com>
2019-06-16 23:16:07 +0900

    CSVファイルを読み込めるようにした CSVファイルを音楽に合わせて読み込めるようにした

---
+++ RhythmGame/Assets/MyPakage/Scripts/NotesMove.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesMove : MonoBehaviour {
+
+    [SerializeField]
+    private float m_MoveTime;
+
+    [SerializeField]
+    private GameObject m_Destination;
+
+    private new Transform transform;
+
+    private float m_Time = 0;
+
+    private Vector3 m_StartPos;
+    private Vector3 m_DestinationPos;
+
+	void Start () {
+        transform = GetComponent<Transform>();
+
+        m_DestinationPos = m_Destination.transform.position;
+        m_StartPos = this.transform.position;
+
+    }
+	
+
+	void Update () {
+        float time = m_Time / m_MoveTime;
+
+        transform.position = Vector3.Lerp(m_StartPos, m_DestinationPos, time);
+
+        m_Time += Time.deltaTime;
+	}
+}
